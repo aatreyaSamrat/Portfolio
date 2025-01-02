85246fb50cc2a88dc29c9df301aad7abb17a58ff
Samrat <paudelsamrat27@gmail.com>
2025-01-03 00:00:18 +0545

    add: Add AppWrap functionality to header and about section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
 import "./Header.scss";
+import { AppWrap } from "../../wrapper";
 
 const ScaleVariants = {
   whileInView: {
@@ -15,7 +16,7 @@ const ScaleVariants = {
 };
 const Header = () => {
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -65,4 +66,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppWrap(Header, "home");
